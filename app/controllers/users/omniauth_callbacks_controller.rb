5e92e91cb34f338a08092245bd1f3394904490df
Chris Marshall <chris@codegoalie.com>
2017-02-01 23:01:23 -0500

    Setup devise and google oauth2 omniauth

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,23 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  before_action :set_user
+
+  def google_oauth2
+    if @user.persisted?
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success',
+                              kind: 'Google'
+      sign_in_and_redirect @user, event: :authentication
+    else
+      # Removing extra as it can overflow some session stores
+      session['devise.google_data'] = \
+        request.env['omniauth.auth'].except(:extra)
+      redirect_to new_user_registration_url,
+                  alert: @user.errors.full_messages.join("\n")
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users,
+             controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
 end
