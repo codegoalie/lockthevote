1aa52e7bee24a27e5e86ab60787554fed8805ad2
Chris Marshall <chris@codegoalie.com>
2017-02-03 13:08:34 -0500

    Add basic election show page

--- app/controllers/elections_controller.rb
+++ app/controllers/elections_controller.rb
@@ -2,4 +2,8 @@ class ElectionsController < ApplicationController
   def index
     @elections = Election.all
   end
+
+  def show
+    @election = Election.includes(:races).find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :elections, only: :index
+  resources :elections, only: [:index, :show]
   devise_for :users,
              controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
