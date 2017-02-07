ffc3868a6051ce0d5c40923a8b21e4a110933cad
Chris Marshall <chris@codegoalie.com>
2017-02-07 18:11:42 -0500

    Send votes to blockchain after casting
    This is pretty naive. We should background this.

--- app/controllers/api/votes_controller.rb
+++ app/controllers/api/votes_controller.rb
@@ -3,7 +3,14 @@ class Api::VotesController < Api::ApplicationController
   def create
     @vote = current_user.votes.create(vote_params)
 
-    unless @vote.valid?
+    if @vote.valid?
+      stub = Votechain::PollingStation::Stub.new('hostmachine.docker:4000',
+                                                 :this_channel_is_insecure)
+      stub.cast(Votechain::Vote.new(voter_proof: current_user.id.to_s,
+                                    race_id: @vote.race.id,
+                                    selection: @vote.selection))
+      render status: :created
+    else
       render status: :bad_request,
              json: { code: 400, messages: @vote.errors.full_messages }
     end
